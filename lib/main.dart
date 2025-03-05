e4af75f63577ea3dc4e9916a7e191a71586f6f2f
Fahri Gani <ganifahri07@gmail.com>
2025-03-05 21:50:08 +0700

    Tampilan dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,56 +10,184 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xFF0A0E14) ,
+        body: Center(
+          child: StatisticsDashboard(),
+        ),
       ),
-      home: const MyHomePage(title: 'Personal Finance Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class StatisticsDashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+    return Container(
+      width: 320,
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Color(0xFF0A0E14),
+        borderRadius: BorderRadius.circular(16),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://storage.googleapis.com/a1aa/image/90vX778wDsyoPaXp-W702YS4aPPVJmUFjBgNLCna0cY.jpg'),
+                radius: 20,
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Anna,',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "it's your statistic",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Icon(
+                Icons.notifications,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildTimeButton('Day'),
+              _buildTimeButton('Week'),
+              _buildTimeButton('Month'),
+              _buildTimeButton('Year'),
+            ],
+          ),
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
             ),
-          ],
+            child: Image.network(
+              'https://storage.googleapis.com/a1aa/image/ywvg2F_uePS04ivarbXjYWyXma3tSVhDGjAXHqtTOGg.jpg',
+              height: 100,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Column(
+              children: [
+                Image.network(
+                  'https://storage.googleapis.com/a1aa/image/sOOgwco8zG5sN9a2hN_WIrfUe5ym9rp4b-LVDts2cPY.jpg',
+                  height: 200,
+                  width: 250,
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 8),
+                Text(
+                  '\$1,499.23',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 16),
+          _buildExpenseItem('Food', Colors.yellow, '\$779.23'),
+          _buildExpenseItem('Subscriptions', Colors.purple, '\$280.00'),
+          _buildExpenseItem('Education', Colors.pink, '\$260.00'),
+          _buildExpenseItem('Beauty', Colors.blue, '\$120.00'),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.home, color: Colors.grey),
+              Icon(Icons.bar_chart, color: Colors.grey),
+              Icon(Icons.account_balance_wallet, color: Colors.grey),
+              Icon(Icons.settings, color: Colors.grey),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimeButton(String text) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      decoration: BoxDecoration(
+        color: Colors.grey[700],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildExpenseItem(String category, Color color, String amount) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                width: 12,
+                height: 12,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+              ),
+              SizedBox(width: 8),
+              Text(
+                category,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+          Text(
+            amount,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+       ),
+          ),
+        ],
       ),
     );
   }
